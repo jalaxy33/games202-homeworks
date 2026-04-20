c8163ba3bc768ce560be6995a8085a12eac98547
jalaxy33 <jalaxy1996@outlook.com>
2026-04-20 14:41:44 +0800

    [HW1] hard shadow map

---
+++ HW1/homework1/src/lights/DirectionalLight.js
@@ -0,0 +1,50 @@
+class DirectionalLight {
+
+    constructor(lightIntensity, lightColor, lightPos, focalPoint, lightUp, hasShadowMap, gl) {
+        this.mesh = Mesh.cube(setTransform(0, 0, 0, 0.2, 0.2, 0.2, 0));
+        this.mat = new EmissiveMaterial(lightIntensity, lightColor);
+        this.lightPos = lightPos;
+        this.focalPoint = focalPoint;
+        this.lightUp = lightUp
+
+        this.hasShadowMap = hasShadowMap;
+        this.fbo = new FBO(gl);
+        if (!this.fbo) {
+            console.log("无法设置帧缓冲区对象");
+            return;
+        }
+    }
+
+    // [HW1] 计算光源的MVP矩阵
+    CalcLightMVP(translate, scale) {
+        let lightMVP = mat4.create();
+        let modelMatrix = mat4.create();
+        let viewMatrix = mat4.create();
+        let projectionMatrix = mat4.create();
+
+        // >> EDIT-START
+        // Model transform
+        mat4.translate(modelMatrix, modelMatrix, translate);
+        mat4.scale(modelMatrix, modelMatrix, scale);
+
+        // View transform
+        mat4.lookAt(viewMatrix, this.lightPos, this.focalPoint, this.lightUp)
+
+        // Projection transform
+        var r = 100;  
+        var l = -r;  
+        var t = 100;  
+        var b = -t;  
+        
+        var n = 0.01;  
+        var f = 200;  
+
+        mat4.ortho(projectionMatrix, l, r, b, t, n, f);
+        // << EDIT-END
+
+        mat4.multiply(lightMVP, projectionMatrix, viewMatrix);
+        mat4.multiply(lightMVP, lightMVP, modelMatrix);
+
+        return lightMVP;
+    }
+}
---
+++ HW1/homework1/src/shaders/phongShader/phongFragment.glsl
@@ -0,0 +1,184 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+// Phong related variables
+uniform sampler2D uSampler;
+uniform vec3 uKd;
+uniform vec3 uKs;
+uniform vec3 uLightPos;
+uniform vec3 uCameraPos;
+uniform vec3 uLightIntensity;
+
+varying highp vec2 vTextureCoord;
+varying highp vec3 vFragPos;
+varying highp vec3 vNormal;
+
+// Shadow map related variables
+#define NUM_SAMPLES 20
+#define BLOCKER_SEARCH_NUM_SAMPLES NUM_SAMPLES
+#define PCF_NUM_SAMPLES NUM_SAMPLES
+#define NUM_RINGS 10
+
+#define EPS 1e-3
+#define PI 3.141592653589793
+#define PI2 6.283185307179586
+
+// [HW1] 阴影贴图相关变量
+// >> EDIT-START
+#define SHADOW_MAP_SIZE 2048.
+#define FRUSTUM_SIZE  400.
+// << EDIT-END
+
+uniform sampler2D uShadowMap;
+
+varying vec4 vPositionFromLight;
+
+highp float rand_1to1(highp float x) { 
+  // -1 -1
+  return fract(sin(x) * 10000.0);
+}
+
+highp float rand_2to1(vec2 uv) { 
+  // 0 - 1
+  const highp float a = 12.9898, b = 78.233, c = 43758.5453;
+  highp float dt = dot(uv.xy, vec2(a, b)), sn = mod(dt, PI);
+  return fract(sin(sn) * c);
+}
+
+float unpack(vec4 rgbaDepth) {
+  // [HW1] 作业框架是除以256，发现阴影会有明显的条纹，改成除以255后问题解决了
+  // >> EDIT-START
+  const vec4 bitShift = vec4(1.0, 1.0 / 255.0, 1.0 / (255.0 * 255.0), 1.0 / (255.0 * 255.0 * 255.0));
+  // << EDIT-END
+  return dot(rgbaDepth, bitShift);
+}
+
+vec2 poissonDisk[NUM_SAMPLES];
+
+void poissonDiskSamples(const in vec2 randomSeed) {
+
+  float ANGLE_STEP = PI2 * float(NUM_RINGS) / float(NUM_SAMPLES);
+  float INV_NUM_SAMPLES = 1.0 / float(NUM_SAMPLES);
+
+  float angle = rand_2to1(randomSeed) * PI2;
+  float radius = INV_NUM_SAMPLES;
+  float radiusStep = radius;
+
+  for(int i = 0; i < NUM_SAMPLES; i++) {
+    poissonDisk[i] = vec2(cos(angle), sin(angle)) * pow(radius, 0.75);
+    radius += radiusStep;
+    angle += ANGLE_STEP;
+  }
+}
+
+void uniformDiskSamples(const in vec2 randomSeed) {
+
+  float randNum = rand_2to1(randomSeed);
+  float sampleX = rand_1to1(randNum);
+  float sampleY = rand_1to1(sampleX);
+
+  float angle = sampleX * PI2;
+  float radius = sqrt(sampleY);
+
+  for(int i = 0; i < NUM_SAMPLES; i++) {
+    poissonDisk[i] = vec2(radius * cos(angle), radius * sin(angle));
+
+    sampleX = rand_1to1(sampleY);
+    sampleY = rand_1to1(sampleX);
+
+    angle = sampleX * PI2;
+    radius = sqrt(sampleY);
+  }
+}
+
+float findBlocker(sampler2D shadowMap, vec2 uv, float zReceiver) {
+  return 1.0;
+}
+
+float PCF(sampler2D shadowMap, vec4 coords) {
+  return 1.0;
+}
+
+float PCSS(sampler2D shadowMap, vec4 coords) {
+
+  // STEP 1: avgblocker depth
+
+  // STEP 2: penumbra size
+
+  // STEP 3: filtering
+
+  return 1.0;
+
+}
+
+// [HW1] stage 1: 硬阴影shadow map
+// << EDIT-START
+// 自适应Shadow Bias算法 https://zhuanlan.zhihu.com/p/370951892
+float getShadowBias(float c, float filterRadiusUV) {
+  vec3 normal = normalize(vNormal);
+  vec3 lightDir = normalize(uLightPos - vFragPos);
+  float fragSize = (1. + ceil(filterRadiusUV)) * (FRUSTUM_SIZE / SHADOW_MAP_SIZE / 2.);
+  return max(fragSize, fragSize * (1.0 - dot(normal, lightDir))) * c;
+}
+
+// 修改了参数，以使用自适应 shadow bias
+float useShadowMap(sampler2D shadowMap, vec4 shadowCoord, float biasC, float filterRadiusUV) {
+  float depth = unpack(texture2D(shadowMap, shadowCoord.xy));
+  float cur_depth = shadowCoord.z;
+  float bias = getShadowBias(biasC, filterRadiusUV);
+  if(cur_depth - bias >= depth + EPS) {
+    return 0.;
+  } else {
+    return 1.0;
+  }
+}
+// >> EDIT-END
+
+vec3 blinnPhong() {
+  vec3 color = texture2D(uSampler, vTextureCoord).rgb;
+  color = pow(color, vec3(2.2));
+
+  vec3 ambient = 0.05 * color;
+
+  vec3 lightDir = normalize(uLightPos);
+  vec3 normal = normalize(vNormal);
+  float diff = max(dot(lightDir, normal), 0.0);
+  vec3 light_atten_coff = uLightIntensity / pow(length(uLightPos - vFragPos), 2.0);
+  vec3 diffuse = diff * light_atten_coff * color;
+
+  vec3 viewDir = normalize(uCameraPos - vFragPos);
+  vec3 halfDir = normalize((lightDir + viewDir));
+  float spec = pow(max(dot(halfDir, normal), 0.0), 32.0);
+  vec3 specular = uKs * light_atten_coff * spec;
+
+  vec3 radiance = (ambient + diffuse + specular);
+  vec3 phongColor = pow(radiance, vec3(1.0 / 2.2));
+  return phongColor;
+}
+
+// [HW1] fs主函数
+// >> EDIT-START
+void main(void) {
+  //vPositionFromLight为光源空间下投影的裁剪坐标，除以w结果为NDC坐标
+  vec3 shadowCoord = vPositionFromLight.xyz / vPositionFromLight.w;
+  //把[-1,1]的NDC坐标转换为[0,1]的坐标
+  shadowCoord = shadowCoord * 0.5 + 0.5;
+
+
+  // 无PCF时的Shadow Bias
+  float bias = .4;
+
+  float visibility = 1.0;
+  // stage 1: 硬阴影shadow map
+  visibility = useShadowMap(uShadowMap, vec4(shadowCoord, 1.0), bias, 0.);
+
+  // visibility = PCF(uShadowMap, vec4(shadowCoord, 1.0));
+  // visibility = PCSS(uShadowMap, vec4(shadowCoord, 1.0));
+
+  vec3 phongColor = blinnPhong();
+
+  gl_FragColor = vec4(phongColor * visibility, 1.0);
+  // gl_FragColor = vec4(phongColor, 1.0);
+}
+// << EDIT-END
